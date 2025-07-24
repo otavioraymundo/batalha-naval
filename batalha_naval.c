46f786b2a8dd260021e6d730e1befe65281f6397
Otavio Raymundo <raymundootavio846@gmail.com>
2025-07-24 09:31:05 -0300

    New feature added

--- batalha_naval.c
+++ batalha_naval.c
@@ -16,6 +16,53 @@ int main() {
             printf("%d ", tabuleiro[i][j]); // Imprime o tabuleiro//
         }
     }
+
+    int octaedro[5][5] = {
+        {0, 0, 1, 0, 0},
+        {0, 1, 1, 1, 0},
+        {1, 1, 1, 1, 1},
+        {0, 1, 1, 1, 0},
+        {0, 0, 1, 0, 0}
+    };
+
+    int cruz[5][5] = {
+        {0, 0, 1, 0, 0},
+        {0, 0, 1, 0, 0},
+        {1, 1, 1, 1, 1},
+        {0, 0, 1, 0, 0},
+        {0, 0, 1, 0, 0}
+    };
+                     
+    int cone[5][5] = {
+        {0, 0, 1, 0, 0},
+        {0, 1, 1, 1, 0},
+        {1, 1, 1, 1, 1},
+        {0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0}
+    };
+
+    printf("\n\nArea de Efeito das abilidades:\n");
+    printf("\nOctaedro:");
+    for (int i = 0; i < 5; i++) {
+        printf("\n");
+        for (int j = 0; j < 5; j++) {
+            printf("%d ", octaedro[i][j]); // Imprime a area de efeito do octaedro//
+        }
+    }
+    printf("\n\nCruz:");
+    for (int i = 0; i < 5; i++) {
+        printf("\n");
+        for (int j = 0; j < 5; j++) {
+            printf("%d ", cruz[i][j]); // Imprime a area de efeito da cruz//
+        }
+    }
+    printf("\n\nCone:");
+    for (int i = 0; i < 5; i++) {
+        printf("\n");
+        for (int j = 0; j < 5; j++) {
+            printf("%d ", cone[i][j]); // Imprime a area de efeito do cone//
+        }
+    }
     
     return 0;
 }
